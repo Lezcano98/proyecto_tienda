4eff1dc140c41efd425b70a8bd27467a39e0f493
Carlos Lezcano <carlos.lezcano988@gmail.com>
2019-08-11 16:36:13 -0600

    siguiente push

--- Cronos/SQLQueryUsuarios.sql
+++ Cronos/SQLQueryUsuarios.sql
@@ -15,15 +15,15 @@ alter procedure SPUsuario
 	@Apellido varchar(20)=null, 
 	@Apellido2 varchar (20)=null,
 	@Cedula varchar(9)= null, 
-	@Correo varchar(20) =null,
+	@Correo varchar(20)=null,
 	@Nombre_Usuario varchar(20)=null,
-	@Clave varbinary(8000)= null,
+	@Clave varchar(50)= null,
 	@tipo varchar(20) =null
 
 	as
 	if @opcion = 1
 	begin
-		insert into Usuario values(@Codigo_Usuario,@Nombre,@Apellido,@Apellido2,@Cedula,@Correo,@Nombre_Usuario, ENCRYPTBYPASSPHRASE('password', @Clave),@tipo)
+		insert into Usuario values(@Codigo_Usuario,@Nombre,@Apellido,@Apellido2,@Cedula,@Correo,@Nombre_Usuario, convert(varchar(20),ENCRYPTBYPASSPHRASE('password', @Clave)),@tipo)
 	end
 
 	if @opcion = 2
@@ -57,3 +57,4 @@ select * from Usuario
 insert into Usuario values(1,'leonardo','rodriguez','salazar','1787822','leo24@selcamome','tomepichi',ENCRYPTBYPASSPHRASE('password','le0'),'Administrador')
 insert into Usuario values(2,'calor','lezcano','montoya','111111','carlos@','lezcano00',ENCRYPTBYPASSPHRASE('password','cl10'),'Cliente')
 -------------------------------------------------------------------------------------------------------------------
+delete Usuario where Codigo_Usuario =2
